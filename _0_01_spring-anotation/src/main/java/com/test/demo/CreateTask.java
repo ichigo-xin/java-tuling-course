7365a16d133bd0ec8993076354011b1d15a587a4
ichigo-xin <1219869429@qq.com>
2024-03-17 16:39:01 +0800

    修改gitignore

---
+++ _0_01_spring-anotation/src/main/java/com/test/demo/CreateTask.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class CreateTask {
+
+    private TaskManageMonitorService taskManageMonitorService;
+}
